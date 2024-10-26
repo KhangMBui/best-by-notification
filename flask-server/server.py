def550e3dd9d78095ca751a4d488f715380cce1e
Khang M Bui <khangbui2002@gmail.com>
2024-10-25 20:02:14 -0700

    Added the sending email functionality. When a product is near-expiration or expired, the system sent an email to the user.

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_mail import Mail, Message
 from flask_cors import CORS
 from datetime import datetime, timedelta
 from db_setup import db
@@ -8,6 +9,17 @@ import threading
 
 app = Flask(__name__)
 CORS(app) # Allows cross-origin requests from React frontend
+mail = Mail(app) # instantiate the mail class 
+
+# configuration of mail 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_DEFAULT_SENDER'] = 'khangbui2023@gmail.com'
+app.config['MAIL_USERNAME'] = 'khangbui2023@gmail.com'
+app.config['MAIL_PASSWORD'] = 'omdt jihh bqho enfc'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app) 
 
 # Config SQLite database
 
@@ -33,11 +45,20 @@ def check_expiring_products():
     for product in products:
       dayLeft = (product.expiration_date - today).days
       if dayLeft <= 0:
-        print(f"Notification: {product.name} is expired. It belong in the street!!!")
+        send_email_notification(product, "expired", "It belongs in the street!!!")
+        print(f"Notification: {product.name} is expired.")
       elif dayLeft <= 2:
+        send_email_notification(product, "expiring soon", "Please cook with it!")
         print(f"Notification: {product.name} is expiring in {dayLeft} days. You better cook with it soon!")
     threading.Timer(86400, check_expiring_products).start()
-    
+
+def send_email_notification(product, status, action):
+  subject = f"Product {product.name} is {status}"
+  body = f"Dear user,\n\nYour product {product.name} is {status}. \n{action}."
+  msg = Message(subject, recipients=["khangbui2002@gmail.com"], body = body)
+  mail.send(msg)
+  return f'Notification email successfully sent for {product.name} ({status})'
+
 # Start the expiration checking thread:
 check_expiring_products()  
 

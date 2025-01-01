529099911f7b31f932f99a8b0a7fb4d293c500a1
Khang Bui <khang.bui@wsu.edu>
2025-01-01 13:05:47 -0800

    Refactored /pages/Welcome: added .css file to it.

--- client/src/pages/InputForm/index.js
+++ client/src/pages/InputForm/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Stylesheet from "reactjs-stylesheet";
 import "./../../assets/fonts/fonts.css";
 import Logo from "../../components/Logo/Logo";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Modal from "../../components/Modal/Modal";
 import "./index.css";
+
 function InputForm() {
   // const [isLinkHovered, setIsLinkHovered] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -112,98 +112,98 @@ function InputForm() {
   );
 }
 
-const styles = Stylesheet.create({
-  appTitleStyle: {
-    fontFamily: "GothicA1-Regular",
-    borderBottom: "3px solid black",
-    fontSize: "8vw",
-  },
-  homeTitleStyle: {
-    fontFamily: "GothicA1-Regular",
-    marginBottom: "5vh",
-    marginTop: "-1vh",
-    fontSize: "8vw",
-  },
-  homeContainerStyle: {
-    alignItems: "center",
-    flexDirection: "column",
-    display: "flex",
-    justifyContent: "center",
-    marginTop: "4vh",
-  },
-  signoutLinkStyle: {
-    marginTop: "44vh",
-    textAlign: "end",
-    color: "#1877F2",
-  },
-  signoutLink: {
-    textDecoration: "none",
-    color: "inherit",
-  },
-  signoutLinkHover: {
-    textDecoration: "underline",
-  },
-  buttonContainer: {
-    marginTop: "4vh",
-    display: "flex",
-    gap: "10px",
-  },
-  actionButton: {
-    padding: "5px 30px",
-    fontSize: "1rem",
-    cursor: "pointer",
-    borderRadius: "12px",
-    border: "1px solid #ccc",
-    backgroundColor: "#000",
-    color: "#FFF",
-    flex: 1,
-  },
-  modalOverlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100vw",
-    height: "100vh",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  modalContainer: {
-    backgroundColor: "#fff",
-    padding: "20px",
-    borderRadius: "8px",
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-  },
-  inputField: {
-    margin: "10px 0",
-    padding: "8px",
-    width: "93%",
-    borderRadius: "8px",
-    border: "1px solid #cccc",
-    fontFamily: "GothicA1-Regular",
-  },
-  modalTitle: {
-    fontFamily: "GothicA1-Regular",
-    fontSize: "5.5vw",
-    textAlign: "center",
-  },
-  modalButtons: {
-    display: "flex",
-    justifyContent: "space-between",
-    marginTop: "10px",
-    gap: "45px",
-  },
-  modalActionButton: {
-    padding: "8px 10px",
-    fontSize: "1rem",
-    cursor: "pointer",
-    borderRadius: "12px",
-    border: "1px solid #ccc",
-    backgroundColor: "#000",
-    color: "#FFF",
-    flex: 1,
-  },
-});
+// const styles = Stylesheet.create({
+//   appTitleStyle: {
+//     fontFamily: "GothicA1-Regular",
+//     borderBottom: "3px solid black",
+//     fontSize: "8vw",
+//   },
+//   homeTitleStyle: {
+//     fontFamily: "GothicA1-Regular",
+//     marginBottom: "5vh",
+//     marginTop: "-1vh",
+//     fontSize: "8vw",
+//   },
+//   homeContainerStyle: {
+//     alignItems: "center",
+//     flexDirection: "column",
+//     display: "flex",
+//     justifyContent: "center",
+//     marginTop: "4vh",
+//   },
+//   signoutLinkStyle: {
+//     marginTop: "44vh",
+//     textAlign: "end",
+//     color: "#1877F2",
+//   },
+//   signoutLink: {
+//     textDecoration: "none",
+//     color: "inherit",
+//   },
+//   signoutLinkHover: {
+//     textDecoration: "underline",
+//   },
+//   buttonContainer: {
+//     marginTop: "4vh",
+//     display: "flex",
+//     gap: "10px",
+//   },
+//   actionButton: {
+//     padding: "5px 30px",
+//     fontSize: "1rem",
+//     cursor: "pointer",
+//     borderRadius: "12px",
+//     border: "1px solid #ccc",
+//     backgroundColor: "#000",
+//     color: "#FFF",
+//     flex: 1,
+//   },
+//   modalOverlay: {
+//     position: "fixed",
+//     top: 0,
+//     left: 0,
+//     width: "100vw",
+//     height: "100vh",
+//     backgroundColor: "rgba(0, 0, 0, 0.5)",
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   modalContainer: {
+//     backgroundColor: "#fff",
+//     padding: "20px",
+//     borderRadius: "8px",
+//     boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+//   },
+//   inputField: {
+//     margin: "10px 0",
+//     padding: "8px",
+//     width: "93%",
+//     borderRadius: "8px",
+//     border: "1px solid #cccc",
+//     fontFamily: "GothicA1-Regular",
+//   },
+//   modalTitle: {
+//     fontFamily: "GothicA1-Regular",
+//     fontSize: "5.5vw",
+//     textAlign: "center",
+//   },
+//   modalButtons: {
+//     display: "flex",
+//     justifyContent: "space-between",
+//     marginTop: "10px",
+//     gap: "45px",
+//   },
+//   modalActionButton: {
+//     padding: "8px 10px",
+//     fontSize: "1rem",
+//     cursor: "pointer",
+//     borderRadius: "12px",
+//     border: "1px solid #ccc",
+//     backgroundColor: "#000",
+//     color: "#FFF",
+//     flex: 1,
+//   },
+// });
 
 export default InputForm;
